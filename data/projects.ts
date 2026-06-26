b026d80e74cb7452631e2542f3c445a9805cf35e
HooshangDev <manouchehr.st+gitHub@gmail.com>
2026-06-26 15:38:32 +0330

    update

--- data/projects.ts
+++ data/projects.ts
@@ -1,8 +1,8 @@
 export const projects = [
   {
-    title: 'RogueRider',
+    title: 'RogueRider(Developed Solo)',
     engine: 'Unreal Engine',
-    description: 'Multiplayer racing game with spline movement and custom replication systems.',
+    description: 'RogueLike Multiplayer racing game with random Road generation system,players have abilities like teleport,speed,rotation and the main goal is to reach the finish point before other players. shop system and cosmetic items are also available. race-pass system and leader board is also implemented. the database used is SQLite Locally on the dedicated server machine. custom physics replication was developed to keep all players updated.',
     image: '/images/projects/rr_garage.jpg',
     screenshots: [
       '/images/projects/rr_screenshot1.jpg',
@@ -14,9 +14,14 @@ export const projects = [
     video: '/videos/projects/rr_video.mp4'
   },
   {
-    title: 'EverRide',
+    title: 'EverRide(Developed Solo)',
     engine: 'Unreal Engine',
-    description: 'Multiplayer racing game with spline movement and custom replication systems.',
+    description: 'Multiplayer racing game with spline movement and custom perfect replication systems.\
+    Custom physics for cars movement and drifting along the road,\
+    3 different abilities to challenge other players, one that switch places, one that slows players on hit, one that boosts the player under some conditions,and we had a shield ability\
+    Database used for this game was also Mysql on the dedicated server machine,and the server itself was Unreal engines dedicated server\
+    shop,leaderboard,cosmetics,customization,custom IAP plugin for a localized market(Bazaar),On-Demand Asset Download & Data Management : unreal engine .pac files\
+    ',
     image: '/images/projects/everride2.jpg',
     screenshots: [
       '/images/projects/everride.jpg',
@@ -40,7 +45,11 @@ export const projects = [
   {
     title: 'Detective Mehrdad',
     engine: 'Unity',
-    description: 'Live ops hidden-object adventure game with backend architecture.',
+    description: 'Live ops hidden-object adventure game with backend architecture.\
+    developed a custom modular Hidden Object game that is used for future events that i developed ,and later will replace the main Hidden Object system(that was not well written and needs replacement)\
+    developed a remote configable daily reward system with a slot machine for rewards\
+    avatar and character customization system were fully developed and migrated server logics and database with the new systems\
+    created a modular sound manager system that used Addressable On-Demand Assed Download and handling sound loops anywhere in the game',
     image: '/images/projects/detective-mehrdad.jpg',
     screenshots: [
       '/images/projects/detective-mehrdad.jpg',
@@ -65,6 +74,37 @@ export const projects = [
       '/images/projects/kalleh2.png'
     ],
     video: '/videos/projects/vr_kalleh.mp4'
+  },
+  {
+    title: 'VR Demo Game(one week project)',
+    engine: 'Unity',
+    description: 'VR Spaceship Prototype (2026)\
+Built within one week.\
+• Modular tutorial framework\
+• Tutorial step completion/failure system\
+• Interactive VR object manipulation\
+• Floating instruction panels\
+• Head-tracked UI system\
+• Draggable tutorial widgets\
+• Bezier-guided object indicators\
+• Sound manager singleton architecture\
+• Physics-based interaction systems\
+• Gameplay: Install spaceship battery\
+• Gameplay: Tighten mechanical components using air wrench\
+• Gameplay: Launch spaceship sequence\
+• Planned Features: Spaceship piloting\
+• Planned Features: Battery management systems\
+• Planned Features: Recharge mechanics\
+',
+    image: '/images/projects/rick0.png',
+    screenshots: [
+      '/images/projects/rick0.png',
+      '/images/projects/rick1.png',
+      '/images/projects/rick3.png',
+      '/images/projects/rick4.png',
+      '/images/projects/rick5.png',
+    ],
+    video: '/videos/projects/VR_test.mp4'
   }
   ,
   {
@@ -77,4 +117,5 @@ export const projects = [
     ],
     video: '/videos/projects/snakeladder.mp4'
   }
+
 ]
